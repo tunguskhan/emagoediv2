edeb1d5d0c890ab525f56b1b2f0c8904e573d8d1
tunguskhan <116023320+tunguskhan@users.noreply.github.com>
2024-04-21 11:35:12 -0700

    Add files via upload

--- modules/config.py
+++ modules/config.py
@@ -564,7 +564,7 @@ def downloading_inpaint_models(v):
     assert v in modules.flags.inpaint_engine_versions
 
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/emagoediv_inpaint/resolve/main/emagoediv_inpaint_head.pth',
+        url='https://huggingface.co/lllyasviel/emagoediv_inpaint/resolve/main/emagoediv_inpaint_head.pth',
         model_dir=path_inpaint,
         file_name='emagoediv_inpaint_head.pth'
     )
@@ -573,7 +573,7 @@ def downloading_inpaint_models(v):
 
     if v == 'v1':
         load_file_from_url(
-            url='https://huggingface.co/tunguskhan/emagoediv_inpaint/resolve/main/inpaint.emagoediv.patch',
+            url='https://huggingface.co/lllyasviel/emagoediv_inpaint/resolve/main/inpaint.emagoediv.patch',
             model_dir=path_inpaint,
             file_name='inpaint.emagoediv.patch'
         )
@@ -581,7 +581,7 @@ def downloading_inpaint_models(v):
 
     if v == 'v2.5':
         load_file_from_url(
-            url='https://huggingface.co/tunguskhan/emagoediv_inpaint/resolve/main/inpaint_v25.emagoediv.patch',
+            url='https://huggingface.co/lllyasviel/emagoediv_inpaint/resolve/main/inpaint_v25.emagoediv.patch',
             model_dir=path_inpaint,
             file_name='inpaint_v25.emagoediv.patch'
         )
@@ -589,7 +589,7 @@ def downloading_inpaint_models(v):
 
     if v == 'v2.6':
         load_file_from_url(
-            url='https://huggingface.co/tunguskhan/emagoediv_inpaint/resolve/main/inpaint_v26.emagoediv.patch',
+            url='https://huggingface.co/lllyasviel/emagoediv_inpaint/resolve/main/inpaint_v26.emagoediv.patch',
             model_dir=path_inpaint,
             file_name='inpaint_v26.emagoediv.patch'
         )
@@ -600,7 +600,7 @@ def downloading_inpaint_models(v):
 
 def downloading_sdxl_lcm_lora():
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/sdxl_lcm_lora.safetensors',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/sdxl_lcm_lora.safetensors',
         model_dir=paths_loras[0],
         file_name=sdxl_lcm_lora
     )
@@ -617,7 +617,7 @@ def downloading_sdxl_lightning_lora():
 
 def downloading_controlnet_canny():
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/control-lora-canny-rank128.safetensors',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/control-lora-canny-rank128.safetensors',
         model_dir=path_controlnet,
         file_name='control-lora-canny-rank128.safetensors'
     )
@@ -626,7 +626,7 @@ def downloading_controlnet_canny():
 
 def downloading_controlnet_cpds():
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/emagoediv_xl_cpds_128.safetensors',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/emagoediv_xl_cpds_128.safetensors',
         model_dir=path_controlnet,
         file_name='emagoediv_xl_cpds_128.safetensors'
     )
@@ -639,14 +639,14 @@ def downloading_ip_adapters(v):
     results = []
 
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/clip_vision_vit_h.safetensors',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/clip_vision_vit_h.safetensors',
         model_dir=path_clip_vision,
         file_name='clip_vision_vit_h.safetensors'
     )
     results += [os.path.join(path_clip_vision, 'clip_vision_vit_h.safetensors')]
 
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/emagoediv_ip_negative.safetensors',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/emagoediv_ip_negative.safetensors',
         model_dir=path_controlnet,
         file_name='emagoediv_ip_negative.safetensors'
     )
@@ -654,7 +654,7 @@ def downloading_ip_adapters(v):
 
     if v == 'ip':
         load_file_from_url(
-            url='https://huggingface.co/tunguskhan/misc/resolve/main/ip-adapter-plus_sdxl_vit-h.bin',
+            url='https://huggingface.co/lllyasviel/misc/resolve/main/ip-adapter-plus_sdxl_vit-h.bin',
             model_dir=path_controlnet,
             file_name='ip-adapter-plus_sdxl_vit-h.bin'
         )
@@ -662,7 +662,7 @@ def downloading_ip_adapters(v):
 
     if v == 'face':
         load_file_from_url(
-            url='https://huggingface.co/tunguskhan/misc/resolve/main/ip-adapter-plus-face_sdxl_vit-h.bin',
+            url='https://huggingface.co/lllyasviel/misc/resolve/main/ip-adapter-plus-face_sdxl_vit-h.bin',
             model_dir=path_controlnet,
             file_name='ip-adapter-plus-face_sdxl_vit-h.bin'
         )
@@ -673,7 +673,7 @@ def downloading_ip_adapters(v):
 
 def downloading_upscale_model():
     load_file_from_url(
-        url='https://huggingface.co/tunguskhan/misc/resolve/main/emagoediv_upscaler_s409985e5.bin',
+        url='https://huggingface.co/lllyasviel/misc/resolve/main/emagoediv_upscaler_s409985e5.bin',
         model_dir=path_upscale_models,
         file_name='emagoediv_upscaler_s409985e5.bin'
     )
